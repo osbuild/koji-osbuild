bbb9acf310017d21246c620ebd61a3dfd47e6630
Christian Kellner <christian@kellner.me>
2020-09-05 18:13:01 +0200

    run-kojid: conditionally exec add-host-to-channel
    Only execute add-host-to-channel if add-host was successful. The
    former throws exceptions.

--- container/builder/run-kojid.sh
+++ container/builder/run-kojid.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-set -eux
+set -ux
 
-koji --server=http://org.osbuild.koji.koji/kojihub \
-     --user=kojiadmin \
-     --password=kojipass \
-     --authtype=password \
-     add-host org.osbuild.koji.kojid i386 x86_64 || true
+KOJI="koji --server=http://org.osbuild.koji.koji/kojihub --user=kojiadmin --password=kojipass --authtype=password"
 
-koji --server=http://org.osbuild.koji.koji/kojihub \
-     --user=kojiadmin \
-     --password=kojipass \
-     --authtype=password \
-     add-host-to-channel org.osbuild.koji.kojid image || true
+$KOJI add-host org.osbuild.koji.kojid i386 x86_64
+
+if [ $? -eq 0 ]; then
+  $KOJI add-host-to-channel org.osbuild.koji.kojid image
+fi
 
 /usr/sbin/kojid -d -v -f --force-lock || cat /var/log/kojid.log
