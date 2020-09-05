5c0965fe9fe50dd0b11208abb8b50adb326d4426
Christian Kellner <christian@kellner.me>
2020-09-05 18:19:55 +0200

    move 'make-tags.sh' to the top-level dir

--- make-tags.sh
+++ make-tags.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/sh
+set -ux
 
 KOJI="koji --server=http://localhost/kojihub --user=kojiadmin --password=kojipass --authtype=password"
 
 $KOJI add-tag f32
 $KOJI add-tag --parent f32 f32-candidate
 $KOJI add-tag --parent f32 --arches=i686,x86_64 f32-build
-$KOJI add-target f32-candidate f32-build f32-canidate
+$KOJI add-target f32-candidate f32-build f32-candidate
+
+$KOJI regen-repo f32-build
