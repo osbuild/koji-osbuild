259756985541c108120f4ffe83939fe4bd6a1e50
Christian Kellner <christian@kellner.me>
2020-09-05 16:31:02 +0200

    container/builder: switch to kerberos auth
    Instead of using client certificates, use kerberos auth.

--- container/builder/run-kojid.sh
+++ container/builder/run-kojid.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -eux
 
-koji --server=http://localhost/kojihub \
+koji --server=http://org.osbuild.koji.koji/kojihub \
      --user=kojiadmin \
      --password=kojipass \
      --authtype=password \
-     add-host kojid i386 x86_64 || true
+     add-host org.osbuild.koji.kojid i386 x86_64 || true
 
-koji --server=http://localhost/kojihub \
+koji --server=http://org.osbuild.koji.koji/kojihub \
      --user=kojiadmin \
      --password=kojipass \
      --authtype=password \
-     add-host-to-channel kojid image || true
+     add-host-to-channel org.osbuild.koji.kojid image || true
 
 /usr/sbin/kojid -d -v -f --force-lock || cat /var/log/kojid.log
