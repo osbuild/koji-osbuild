7bd3f197a70fb4d4985f1be17152bfd7b59526df
Christian Kellner <christian@kellner.me>
2020-09-05 18:14:44 +0200

    run-kojid: also add builder to createrepo channel
    At least one builder needs to be able to create repos.

--- container/builder/run-kojid.sh
+++ container/builder/run-kojid.sh
@@ -7,6 +7,7 @@ $KOJI add-host org.osbuild.koji.kojid i386 x86_64
 
 if [ $? -eq 0 ]; then
   $KOJI add-host-to-channel org.osbuild.koji.kojid image
+  $KOJI add-host-to-channel org.osbuild.koji.kojid createrepo
 fi
 
 /usr/sbin/kojid -d -v -f --force-lock || cat /var/log/kojid.log
