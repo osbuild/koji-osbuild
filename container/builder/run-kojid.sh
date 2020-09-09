b778133e89aba5f472ddc9fce2d3492521f47dc1
Christian Kellner <christian@kellner.me>
2020-09-10 00:35:13 +0200

    container: support installing via RPMs
    If the share dir contains rpms in /share/rpms, install those,
    instead of using the plugin from the container.

--- container/builder/run-kojid.sh
+++ container/builder/run-kojid.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -ux
 
+if ls /share/rpms/*.rpm 1> /dev/null 2>&1; then
+  echo "Using RPMs"
+  rm /usr/lib/koji-builder-plugins/osbuild.py
+  rpm -i /share/rpms/koji-osbuild-?-0.*.rpm \
+         /share/rpms/koji-osbuild-builder-*.rpm
+fi
+
 KOJI="koji --server=http://org.osbuild.koji.koji/kojihub --user=kojiadmin --password=kojipass --authtype=password"
 
 $KOJI add-host org.osbuild.koji.kojid i386 x86_64
