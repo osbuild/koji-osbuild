5c6605b974db6298364bcd241e2e4cce0411aaac
Christian Kellner <christian@kellner.me>
2020-09-17 16:53:14 +0200

    test: shellcheck support for run-tests.sh
    The severity is set to "warning" by default, but can be controlled
    via the SHELLCHECK_SEVERITY env variable.

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 set -euo pipefail
 
+SHELLCHECK_SEVERITY=${SHELLCHECK_SEVERITY:-warning}
 
 run_test() {
   podman run -it -v "$(pwd)":/github/workspace:z --env "GITHUB_WORKSPACE=/github/workspace" koji.test "$1"
@@ -10,5 +11,8 @@ pushd test
 podman build -t koji.test -f Dockerfile .
 popd
 
+SCRIPTS="$(git ls-files --exclude='*.sh' --ignored | xargs echo)"
+
+run_test "shellcheck -S ${SHELLCHECK_SEVERITY} ${SCRIPTS}"
 run_test "pytest -v --cov-report=term --cov=osbuild test/unit/"
 run_test "pylint plugins/**/*.py test/**/*.py"
