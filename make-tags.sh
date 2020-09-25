65bc71c9d1bbc03fca5b296dd82640fc50a69fd3
Christian Kellner <christian@kellner.me>
2020-09-25 14:19:50 +0200

    make-tags.sh: add packages to tag
    Add the "Fedora-Cloud" and "RHEL-Cloud" package to the f32-candidate
    tag, so that builds can be tagged properly.

--- make-tags.sh
+++ make-tags.sh
@@ -8,4 +8,7 @@ $KOJI add-tag --parent f32 f32-candidate
 $KOJI add-tag --parent f32 --arches=x86_64 f32-build
 $KOJI add-target f32-candidate f32-build f32-candidate
 
+$KOJI add-pkg --owner kojiadmin f32-candidate Fedora-Cloud
+$KOJI add-pkg --owner kojiadmin f32-candidate RHEL-Cloud
+
 $KOJI regen-repo f32-build
