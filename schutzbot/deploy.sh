cd1928d8f3da6b180fa7f187d083552ff71f6dda
Christian Kellner <christian@kellner.me>
2022-01-27 17:24:29 +0000

    schutzbot: update osbuild to 46
    osbuild 30 is quite old, use a new one.

--- schutzbot/deploy.sh
+++ schutzbot/deploy.sh
@@ -20,7 +20,7 @@ function retry {
 
 # Variables for where to find osbuild-composer RPMs to test against
 DNF_REPO_BASEURL=http://osbuild-composer-repos.s3-website.us-east-2.amazonaws.com
-OSBUILD_COMMIT=35de3093a7b52569512bdc61d2105febbb9b0c7e             # release 30
+OSBUILD_COMMIT=bb30ffa0629e16ecff103aaaeb7e931f3f8ff79e             # release 46
 OSBUILD_COMPOSER_COMMIT=b5987a5ca51826f29a3bce742d693a55f16f016f    # commit newer than release 30 (we need one with rhel-8-cdn)
 
 # Get OS details.
